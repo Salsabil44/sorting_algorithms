8acef089728c0011fc1ab0517abbbd2fce2eb580
Salsabil44 <achisalsabil556@gmail.com>
2024-01-23 18:18:38 -0800

    algo

---
+++ 105-radix_sort.c
@@ -0,0 +1,26 @@
+#include "sort.h"
+
+int get_max(int *array, int size);
+void radix_counting_sort(int *array, size_t size, int sig, int *buff);
+void radix_sort(int *array, size_t size);
+
+/**
+ * get_max - Get the maximum value in an array of integers.
+ * @array: An array of integers.
+ * @size: The size of the array.
+ *
+ * Return: The maximum integer in the array.
+ * Author: Sally
+ */
+int get_max(int *array, int size)
+{
+	int max, i;
+
+	for (max = array[0], i = 1; i < size; i++)
+	{
+		if (array[i] > max)
+			max = array[i];
+	}
+
+	return (max);
+}
